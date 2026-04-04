be93116d79787ab338fd12642bef780b66c9a343
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2026-04-04 17:18:11 +0300

    fix

--- src/utils/checkInternetProvider.tsx
+++ src/utils/checkInternetProvider.tsx
@@ -28,6 +28,13 @@ export default function CheckInternetProvider({
   if (Online) {
     return <>{children}</>;
   } else {
-    router.push("/no_internet");
+    return (
+      <div className="flex items-center justify-center p-5 min-h-screen">
+        <h1 className="text-2xl sm:text-4xl text-center">
+          failed to load data , please check your internet connection and try
+          again
+        </h1>
+      </div>
+    );
   }
 }
